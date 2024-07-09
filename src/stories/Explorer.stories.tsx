d0104a984dabf9a1bd0080d8250ebc8a7354635f
Chamara Senarath <senarath.chamara@gmail.com>
2024-07-09 19:53:19 +0100

    init

---
+++ src/stories/Explorer.stories.tsx
@@ -0,0 +1,11 @@
+import Explorer from "../components/Explorer";
+
+export default {
+  component: Explorer,
+  title: "Explorer",
+  tags: ["autodocs"],
+};
+
+export const Default = {
+  args: {},
+};
