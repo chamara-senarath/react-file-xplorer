eb5f990004bb648c6e97e7cabeb789b0a06d356f
Chamara Senarath <senarath.chamara@gmail.com>
2024-07-13 16:16:53 +0100

    Added icons prop to pass icons and style changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,86 @@
-import "./App.css";
+import Explorer from "./components/Explorer";
+
+const explorer = {
+  id: "1",
+  name: "root",
+  isFolder: true,
+  items: [
+    {
+      id: "2",
+      name: "public",
+      isFolder: true,
+      items: [
+        {
+          id: "3",
+          name: "public nested 1",
+          isFolder: true,
+          items: [
+            {
+              id: "4",
+              name: "index.html",
+              isFolder: false,
+              items: [],
+            },
+            {
+              id: "5",
+              name: "hello.html",
+              isFolder: false,
+              items: [],
+            },
+          ],
+        },
+        {
+          id: "6",
+          name: "public_nested_file",
+          isFolder: false,
+          items: [],
+        },
+      ],
+    },
+    {
+      id: "7",
+      name: "src",
+      isFolder: true,
+      items: [
+        {
+          id: "8",
+          name: "App.js",
+          isFolder: false,
+          items: [],
+        },
+        {
+          id: "9",
+          name: "Index.js",
+          isFolder: false,
+          items: [],
+        },
+        {
+          id: "10",
+          name: "styles.css",
+          isFolder: false,
+          items: [],
+        },
+      ],
+    },
+    {
+      id: "11",
+      name: "package.json",
+      isFolder: false,
+      items: [],
+    },
+  ],
+};
 
 function App() {
   return (
-    <>
-      <h2>Hello</h2>
-    </>
+    <div className="flex gap-x-2">
+      <div className="bg-gray-100 rounded-lg min-h-screen p-2 w-1/4">
+        <Explorer explorer={explorer} />
+      </div>
+      <div className="flex flex-col w-full items-center p-2">
+        <h1 className="font-bold text-2xl text-teal-400">React File Xplorer</h1>
+      </div>
+    </div>
   );
 }
 
--- src/components/Explorer.tsx
+++ src/components/Explorer.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import { MouseEventHandler, useMemo, useState } from "react";
 import { classNames } from "../util";
-import chevronDownIcon from "../assets/chevron-down.svg";
-import chevronUpIcon from "../assets/chevron-up.svg";
-import folderOpenIcon from "../assets/folder-open.svg";
-import folderClosedIcon from "../assets/folder-closed.svg";
-import fileIcon from "../assets/file.svg";
+import defaultChevronDownIcon from "../assets/chevron-down.svg";
+import defaultChevronUpIcon from "../assets/chevron-up.svg";
+import defaultFolderOpenIcon from "../assets/folder-open.svg";
+import defaultFolderClosedIcon from "../assets/folder-closed.svg";
+import defaultFileIcon from "../assets/file.svg";
 
 type FileStructure = {
   id: string;
@@ -16,36 +16,114 @@ type FileStructure = {
 
 interface Props {
   explorer: FileStructure;
+  icons?: {
+    size?: number;
+    fileIcon?: string;
+    folderOpenIcon?: string;
+    folderClosedIcon?: string;
+    chevronUpIcon?: string;
+    chevronDownIcon?: string;
+  };
+  level?: number;
 }
 
-const Explorer = ({ explorer }: Props) => {
+const DEFAULT_ICON_SIZE = 14;
+
+const VerticalLine = ({ space }: { space: number }) => {
+  return (
+    <span
+      className="absolute border-[0.5px] border-gray-300 h-full z-10"
+      style={{
+        left: `${space * 4}px`,
+      }}
+    ></span>
+  );
+};
+
+const Explorer = ({ explorer, icons, level = 0 }: Props) => {
   const [expand, setExpand] = useState(false);
+
+  const getIcon = (
+    localIcon: string | undefined,
+    globalIcon: string | undefined,
+    defaultIcon: string
+  ) => {
+    return localIcon ?? globalIcon ?? defaultIcon;
+  };
+
+  const fileIcon = useMemo(() => {
+    return getIcon(explorer.icon, icons?.fileIcon, defaultFileIcon);
+  }, [explorer.icon, icons?.fileIcon]);
+
+  const folderOpenIcon = useMemo(
+    () => getIcon(explorer?.icon, icons?.folderOpenIcon, defaultFolderOpenIcon),
+    [explorer?.icon, icons?.folderOpenIcon]
+  );
+
+  const folderClosedIcon = useMemo(
+    () =>
+      getIcon(explorer?.icon, icons?.folderClosedIcon, defaultFolderClosedIcon),
+    [explorer?.icon, icons?.folderClosedIcon]
+  );
+
+  const chevronUpIcon = icons?.chevronUpIcon ?? defaultChevronUpIcon;
+  const chevronDownIcon = icons?.chevronDownIcon ?? defaultChevronDownIcon;
+
+  const iconSize = icons?.size ?? DEFAULT_ICON_SIZE;
 
   const handleOnClick = () => {
     console.log(explorer.id);
   };
 
+  const onClickChevron: MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setExpand(!expand);
+  };
+
   // Return folder
   if (explorer.isFolder) {
     return (
-      <div className="flex flex-col">
-        <div className="flex justify-between" onClick={handleOnClick}>
-          <span className="flex w-full gap-x-1 items-center select-none	cursor-pointer">
-            <img src={expand ? folderOpenIcon : folderClosedIcon} width={12} />
+      <div className="flex flex-col relative">
+        {level > 0 && <VerticalLine space={level} />}
+        <div
+          className="flex justify-between px-2 py-1 hover:bg-gray-50 rounded-lg"
+          onClick={handleOnClick}
+        >
+          <span
+            className="flex w-full gap-x-1 items-center select-none	cursor-pointer"
+            style={{
+              paddingLeft: `${level * 4}px`,
+            }}
+          >
+            <img
+              src={expand ? folderOpenIcon : folderClosedIcon}
+              width={iconSize}
+            />
             {explorer.name}
           </span>
           <button
-            className="select-none	cursor-pointer"
-            onClick={() => setExpand(!expand)}
+            className="select-none cursor-pointer"
+            onClick={onClickChevron}
           >
-            <img src={expand ? chevronUpIcon : chevronDownIcon} width={12} />
+            <img
+              src={expand ? chevronUpIcon : chevronDownIcon}
+              width={iconSize}
+            />
           </button>
         </div>
         <div
-          className={classNames(expand ? "flex flex-col" : "hidden", "pl-2")}
+          className={classNames(expand ? "flex flex-col" : "hidden", "py-1")}
         >
           {explorer.items.map((item) => {
-            return <Explorer explorer={item} key={item.id} />;
+            return (
+              <Explorer
+                explorer={item}
+                icons={icons}
+                key={item.id}
+                level={level + 4}
+              />
+            );
           })}
         </div>
       </div>
@@ -54,12 +132,21 @@ const Explorer = ({ explorer }: Props) => {
 
   // Return file
   return (
-    <span
-      onClick={handleOnClick}
-      className="flex gap-x-1 select-none	cursor-pointer"
+    <div
+      className="flex hover:bg-gray-50 rounded-lg px-2 relative"
+      style={{
+        paddingLeft: `${level * 4}px`,
+      }}
     >
-      <img src={fileIcon} width={12} /> {explorer.name}
-    </span>
+      <VerticalLine space={level} />
+      <div
+        className="flex w-full select-none cursor-pointer px-2 py-1"
+        onClick={handleOnClick}
+      >
+        <img src={fileIcon} width={iconSize} />
+        <span>{explorer.name}</span>
+      </div>
+    </div>
   );
 };
 
