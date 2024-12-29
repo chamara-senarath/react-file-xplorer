29f7ca8f870a6c1ca031855af7e0a5d52e4ac00b
Chamara Senarath <senarath.chamara@gmail.com>
2024-12-29 19:37:20 +0000

    Updates to website

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ import Explorer from "./components/Explorer";
 import GitHubIcon from "./assets/github.svg";
 import NpmIcon from "./assets/npm.svg";
 import Logo from "./assets/logo.svg";
+import { useState } from "react";
+import FeatureNavigationIcon from "./assets/feature-navigation.svg";
+import FeatureCustomizableIconsIcon from "./assets/feature-customizable-icons.svg";
+import FeatureTypeScriptSupportIcon from "./assets/feature-ts-support.svg";
+import FeatureEventHandlingIcon from "./assets/feature-event-handling.svg";
+
 const fileStructure = {
   id: "1",
   name: "root",
@@ -73,70 +79,135 @@ const fileStructure = {
   ],
 };
 
+const infoCardsData = [
+  {
+    title: "Intuitive Navigation",
+    description:
+      "Clean hierarchical display of files and folders with expandable/collapsible directories",
+    icon: FeatureNavigationIcon,
+  },
+  {
+    title: "Fully Customizable Icons",
+    description:
+      "Support for custom icons for files, folders (open/closed states), and chevrons",
+    icon: FeatureCustomizableIconsIcon,
+  },
+  {
+    title: "TypeScript Support",
+    description: "Fully typed component for better development experience",
+    icon: FeatureTypeScriptSupportIcon,
+  },
+  {
+    title: "Event Handling",
+    description:
+      "Built-in click handlers for both files and folders with customizable callbacks",
+    icon: FeatureEventHandlingIcon,
+  },
+];
+
 function App() {
+  const [currentId, setCurrentId] = useState("1");
+
+  const flattenStructure = (structure: any) => {
+    const result: any = [];
+
+    function traverse(node: any) {
+      result.push({
+        id: node.id,
+        name: node.name,
+        isFolder: node.isFolder,
+      });
+
+      if (node.items && node.items.length > 0) {
+        node.items.forEach(traverse);
+      }
+    }
+
+    traverse(structure);
+
+    return result;
+  };
+
+  const flatStructure = flattenStructure(fileStructure);
+
   const getFileNameById = (id: string) => {
-    return fileStructure.items.find((item) => item.id === id)?.name;
+    return flatStructure.find((item: any) => item.id === id)?.name;
   };
 
   const handleItemClick = (id: string) => {
-    console.log(id);
-    console.log(getFileNameById(id));
+    setCurrentId(id);
   };
 
-  const InfoCard = () => {
+  const InfoCard = ({
+    title,
+    description,
+    icon,
+  }: {
+    title: string;
+    description: string;
+    icon: string;
+  }) => {
     return (
       <div className="bg-white rounded-lg shadow-md p-4 max-w-sm">
         <div className="flex items-center space-x-3">
-          <div className="bg-blue-100 p-2 rounded-full w-10 h-10 shrink-0 text-center">
-            🚀
+          <div className="bg-blue-100 p-2 rounded-full w-10 h-10 shrink-0">
+            <img src={icon} alt={`feature-${title}`} />
           </div>
           <div>
-            <h3 className="text-md font-semibold text-gray-900">
-              Getting Started
-            </h3>
-            <p className="text-sm text-gray-500">
-              From enrolling your student in Juni to taking your first class
-            </p>
+            <h3 className="text-md font-semibold text-gray-900">{title}</h3>
+            <p className="text-sm text-gray-500">{description}</p>
           </div>
         </div>
       </div>
     );
   };
 
+  const isFolder = (id: string) => {
+    return flatStructure.find((item: any) => item.id === id)?.isFolder;
+  };
+
   return (
     <div className="flex flex-col w-full h-screen">
       {/* header */}
-      <header className="flex justify-between items-center border-b-[0.5px] border-gray-200 px-8 py-4">
+      <header className="flex justify-between items-center border-b-[0.5px] border-gray-200 px-[10vw] py-4">
         <div className="flex items-center justify-center gap-x-2">
-          <img className="w-6" src={Logo} alt="react-file-xplorer-icon" />
-          <h1 className="font-normal text-blue-900 text-2xl">
+          <img className="w-8" src={Logo} alt="react-file-xplorer-icon" />
+          <h1 className="font-normal text-[#2563EB] text-2xl">
             React File Xplorer
           </h1>
         </div>
         <div className="flex gap-x-4">
-          <button className="flex items-center justify-center rounded-full w-8 h-8 bg-white border-[0.5px] border-blue-900">
+          <a
+            href=""
+            className="flex items-center justify-center rounded-full w-8 h-8 bg-white border-[0.5px] border-[#2563EB]/20 hover:bg-[#2563EB]/10"
+          >
             <img className="w-4" src={GitHubIcon} alt="github-icon" />
-          </button>
-          <button className="flex items-center justify-center rounded-full w-8 h-8 bg-white border-[0.5px] border-blue-900">
+          </a>
+          <a className="flex items-center justify-center rounded-full w-8 h-8 bg-white border-[0.5px] border-[#2563EB]/20 hover:bg-[#2563EB]/10">
             <img className="w-4" src={NpmIcon} alt="npm-icon" />
-          </button>
+          </a>
         </div>
       </header>
 
       {/* body */}
-      <main className="flex flex-col items-center gap-y-8 p-8 mb-auto">
-        <p className="text-gray-700 text-sm">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-          ipsam iure numquam perferendis illum, veritatis eius rerum laudantium!
-          Maiores, consectetur unde. Ea quae laborum officiis facilis id esse?
-          Optio, aliquam?
+      <main className="flex flex-col gap-y-8 py-8 px-[10vw] mb-auto">
+        <p className="text-gray-700 text-sm md:text-md">
+          A lightweight, customizable file explorer component for React
+          applications that provides an intuitive interface for displaying
+          hierarchical file and folder structures. This component offers a
+          clean, modern design with smooth interactions and extensive
+          customization options.
         </p>
         <div className="flex flex-col items-center gap-4 w-full lg:flex-row lg:items-start lg:h-full">
-          <div className="flex flex-wrap justify-center gap-4 lg:w-1/3">
-            <InfoCard />
-            <InfoCard />
-            <InfoCard />
-            <InfoCard />
+          <div className="flex flex-wrap justify-center gap-4 lg:w-1/3 lg:justify-start">
+            {infoCardsData.map((card, index) => (
+              <InfoCard
+                key={index}
+                title={card.title}
+                description={card.description}
+                icon={card.icon}
+              />
+            ))}
           </div>
 
           <div className="flex flex-col w-full lg:w-2/3 h-[432px]">
@@ -145,15 +216,27 @@ function App() {
               <span className="w-3 h-3 rounded-full bg-yellow-400"></span>
               <span className="w-3 h-3 rounded-full bg-green-400"></span>
             </div>
-            <div className="flex bg-gray-100 rounded-b-lg w-full p-4 h-full gap-x-4 overflow-hidden">
-              <div className="bg-white rounded-lg p-2 w-72 shadow-sm overflow-hidden">
+            <div className="flex flex-col md:flex-row bg-gray-100 rounded-b-lg w-full p-4 h-full gap-4 overflow-hidden">
+              <div className="flex shrink-0 bg-white rounded-lg p-2 min-w-[220px] max-h-[200px] md:max-h-full h-full shadow-sm overflow-auto">
                 <Explorer
                   fileStructure={fileStructure}
                   onItemClick={handleItemClick}
+                  isRootExpanded={true}
                 />
               </div>
-              <div className="bg-white w-full rounded-lg shadow-sm p-2">
-                <p>hello</p>
+              <div className="flex flex-col justify-center items-center bg-white w-full h-full rounded-lg shadow-sm p-2">
+                <h1 className="text-2xl md:text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-600">
+                  {currentId === "1"
+                    ? "Let's Play with Files"
+                    : getFileNameById(currentId)}
+                </h1>
+                <p className="text-xl md:text-2xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-gray-200 to-gray-400">
+                  {currentId === "1"
+                    ? "Click on a file to get started"
+                    : isFolder(currentId)
+                    ? "folder"
+                    : "file"}
+                </p>
               </div>
             </div>
           </div>
@@ -161,10 +244,18 @@ function App() {
       </main>
 
       {/* footer */}
-      <footer className="flex justify-between items-center border-t-[0.5px] border-gray-200 px-8 py-4">
-        <p className="text-gray-700 text-sm">
-          Made with &#9829; and creativity by Chamara Senarath
-        </p>
+      <footer className="flex w-full justify-center items-center border-t-[0.5px] border-gray-200 px-8 py-4">
+        <span className="text-gray-700 text-sm text-center">
+          Made with &#9829; and creativity by{" "}
+          <a
+            className="text-[#2563EB]/80"
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.chamarasenarath.com"
+          >
+            Chamara Senarath
+          </a>
+        </span>
       </footer>
     </div>
   );
